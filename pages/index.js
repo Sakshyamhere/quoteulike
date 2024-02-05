17e073527b7825b2cbf7655968fbdc087683eb03
Sakshyamhere <aryalsakshyam33@gmail.com>
2024-02-05 21:50:50 +0545

    Random quote generator

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,120 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Loading from "@/components/Loading";
+import autoprefixer from "autoprefixer";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [canvas, setCanvas] = useState(false)
+  const genWithRandomQuote = async () => {
+    setCanvas(true)
+    setLoading(true);
+    const data = await fetch("https://api.quotable.io/random");
+    const parsedData = await data.json();
+    const quoteData = parsedData.content;
+    const authorData = parsedData.author;
+    draw(quoteData, authorData);
+  };
+  const genWithGivenQuote = () => {
+    if (quote.length >= 5) {
+    setCanvas(true)
+      setError("");
+      setLoading(true);
+      draw(quote, author);
+    } else {
+      setError("Enter quote");
+    }
+  };
+  const draw = (quote, author) => {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    // Create an image object
+    const img = new Image();
+    img.src = "https://picsum.photos/1200/900";
+
+    img.onload = () => {
+      // Draw the image on the canvas
+      ctx.drawImage(img, 0, 0);
+
+      // Add text to the canvas
+      ctx.font = "20px serif";
+      ctx.fillText(quote, 50, 90);
+      if (author.length !== 0) {
+        ctx.fillText(`-${author}`, 50, 130);
+      }
+    };
+    setLoading(false);
+  };
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div>
+      <span className="text-center">
+        <p className="text-5xl py-4">QuoteGen</p>
+      </span>
+      <div className="flex justify-center my-10">
+        <div className="flex flex-col border border-gray-500 shadow-md rounded-sm w-[90%] md:w-1/2">
+          <div className="p-4 w-full">
+            <label htmlFor="quote" className="text-2xl">
+              Enter Quote
+            </label>
+            <input
+              type="text"
+              name="quote"
+              id="quote"
+              className="w-full py-2 px-1 text-gray-200 rounded-sm bg-gray-700 my-2 mb-5"
+              placeholder="Enter Quote"
+              onChange={(e) => {
+                setQuote(e.target.value);
+              }}
+              required
             />
-          </a>
+            <label htmlFor="quoteAuthor" className="text-2xl">
+              Enter Author
+            </label>
+            <input
+              type="text"
+              name="quoteAuthor"
+              id="quoteAuthor"
+              className="w-full py-2 px-1 text-gray-200 rounded-sm bg-gray-700 my-2"
+              placeholder="Enter Author(optional)"
+              onChange={(e) => {
+                setAuthor(e.target.value);
+              }}
+            />
+            {error.length !== 0 && (
+              <p id="error" className="w-full p-3 text-center bg-red-700">
+                {error}
+              </p>
+            )}
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex justify-center my-10">
+        <div className="flex md:flex-col border border-gray-500 shadow-md rounded-sm w-[90%] md:w-1/2">
+          <div className="p-4 w-full text-center flex flex-col md:flex-row">
+            <button
+              className="p-3 rounded-sm w-full my-2 md:mx-2 mx-auto bg-gray-300"
+              onClick={genWithGivenQuote}
+            >
+              Generate Image with given quote
+            </button>
+            <button
+              className="p-3 rounded-sm w-full my-2 md:mx-2 mx-auto  bg-gray-300"
+              onClick={genWithRandomQuote}
+            >
+              Generate Image with random quote
+            </button>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex justify-center my-10">
+        {loading && <Loading />}
+        {canvas &&
+        <canvas id="canvas" width={1000} height={1000}></canvas>
+        }
       </div>
-    </main>
+    </div>
   );
 }
